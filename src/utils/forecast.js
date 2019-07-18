b1ecb5518aa59e40f698b41150d8acee34d6685d
Victor Uemura <uemura.v@gmail.com>
2019-07-17 19:59:59 -0700

    removed a line

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unble to find location!', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. \nThere is a ' + body.currently.precipProbability + '% chance of rain. \nThe high is ' + body.daily.data[0].temperatureHigh + '\nWith a low of ' + body.daily.data[0].temperatureLow + 'Mariana is a fart-face')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. \nThere is a ' + body.currently.precipProbability + '% chance of rain. \nThe high is ' + body.daily.data[0].temperatureHigh + '\nWith a low of ' + body.daily.data[0].temperatureLow)
         }
     })
 }
